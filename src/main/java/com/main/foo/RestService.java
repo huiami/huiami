a48b496148a711895e1b3d86ef3de3900e13c26f
Huiami <359888@gmail.com>
2014-03-24 03:43:15 +0200

    a

---
+++ src/main/java/com/main/foo/RestService.java
@@ -0,0 +1,22 @@
+package com.main.foo;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/service")
+@RequestScoped
+public class RestService {
+	 @GET
+	    @Produces(MediaType.TEXT_PLAIN)
+	    public String test() {
+	        return "HA";
+	    }
+
+}
